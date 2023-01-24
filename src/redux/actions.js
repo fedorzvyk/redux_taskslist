763f28fe675dd5b1a5e6e700bdb0f5c7c63c4c91
fedorzvyk <fedorzvyk@gmail.com>
2023-01-24 15:50:53 +0200

    refactored with Redux Toolkit

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,33 +1,57 @@
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
+import { createAction, nanoid } from "@reduxjs/toolkit";
 
-export const addTask = text => {
-  return {
-    type: "tasks/addTask",
-    payload: {
-      id: nanoid(),
-      completed: false,
-      text,
-    },
-  };
-};
+// export const addTask = text => {
+//   return {
+//     type: "tasks/addTask",
+//     payload: {
+//       id: nanoid(),
+//       completed: false,
+//       text,
+//     },
+//   };
+// };
 
-export const deleteTask = taskId => {
-  return {
-    type: "tasks/deleteTask",
-    payload: taskId,
-  };
-};
 
-export const toggleCompleted = taskId => {
-  return {
-    type: "tasks/toggleCompleted",
-    payload: taskId,
-  };
-};
 
-export const setStatusFilter = value => {
-  return {
-    type: "filters/setStatusFilter",
-    payload: value,
-  };
-};
+
+// console.log(addTask("Learn Redux Toolkit"));
+// {type: "tasks/addTask", payload: "Learn Redux Toolkit"}
+
+// export const deleteTask = taskId => {
+//   return {
+//     type: "tasks/deleteTask",
+//     payload: taskId,
+//   };
+// };
+
+// export const toggleCompleted = taskId => {
+//   return {
+//     type: "tasks/toggleCompleted",
+//     payload: taskId,
+//   };
+// };
+
+// export const setStatusFilter = value => {
+//   return {
+//     type: "filters/setStatusFilter",
+//     payload: value,
+//   };
+// };
+
+
+
+
+
+export const addTask = createAction("tasks/AddTask", text => {
+    return {
+      payload: {
+        text,
+        id: nanoid(),
+        completed: false,
+      },
+    };
+  });
+export const deleteTask = createAction("tasks/deleteTask");
+export const toggleCompleted = createAction("tasks/toggleCompleted");
+export const setStatusFilter = createAction("filters/setStatusFilter");
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,18 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from "./reducer";
-// Початкове значення стану Redux для кореневого редюсера,
-// якщо не передати параметр preloadedState.
-// const initialState = {
-//   tasks: [
-//     { id: 0, text: "Learn HTML and CSS", completed: true },
-//     { id: 1, text: "Get good at JavaScript", completed: true },
-//     { id: 2, text: "Master React", completed: false },
-//     { id: 3, text: "Discover Redux", completed: false },
-//     { id: 4, text: "Build amazing apps", completed: false },
-//   ],
-//   filters: {
-//     status: "all",
-//   },
-// };
+// import { createStore } from "redux";
+// import { devToolsEnhancer } from "@redux-devtools/extension";
+// import { rootReducer } from "./reducer";
+// import { tasksReducer, filtersReducer } from "./reducer";
+import { configureStore } from "@reduxjs/toolkit";
+import { tasksReducer } from "./tasksSlice";
+import { filtersReducer } from "./filtersSlice";
 
+// const enhancer = devToolsEnhancer();
 
-// Створюємо розширення стора, щоб додати інструменти розробника
-const enhancer = devToolsEnhancer();
+// export const store = createStore(rootReducer, enhancer);
 
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+    reducer: {
+        tasks: tasksReducer,
+        filters: filtersReducer,
+      },
+  });
